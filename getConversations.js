70013b47192b8a550c096a1513a7b59a994aefe4
Henry Hoggard <henry@henryhoggard.co.uk>
2013-02-04 13:30:01 +0000

    Get messages working

--- getConversations.js
+++ getConversations.js
@@ -1,8 +1,17 @@
+var table ="<table class=\"table\"   >  <thead>  <tr>  <th>From</th>  <th>Subject</th>   <th>Date</th>  </tr>  </thead>  <tbody> ";
 $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
+    $.each(json, function(arrayID,message) {
+        $.each(message, function() {
+            table += "<tr><td><a href=\"#" + message.conversation.id + "\">test</a></td>";
+            table += "<td>" + message.conversation.subject + "</td>";
+            table += "<td>" + message.conversation.updated_at + "</td></tr>";
+        });
+    });
+    table += "</tbody></table";
 
-$.each(json, function(arrayID,message) {
-    $.each(message.conversation, function(eventID,eventData) {
-            console.log(message.conversation.subject);
-     });
+  document.getElementById('table1').innerHTML = table;
+
+
 });
-});
+
+
