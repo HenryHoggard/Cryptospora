d9b0339c932c7b028a57ccbb78242774ce4d1f88
Henry Hoggard <henry@henryhoggard.co.uk>
2013-02-07 10:02:31 +0000

    First Week Release

--- getConversations.js
+++ getConversations.js
@@ -1,8 +1,10 @@
-var table ="<table class=\"table\"   >  <thead>  <tr>  <th>From</th>  <th>Subject</th>   <th>Date</th>  </tr>  </thead>  <tbody> ";
+var table ="<table class=\"table\" data-provides=\"rowlink\"  >  <thead>  <tr><th></th>  <th>From</th>  <th>Subject</th>   <th>Date</th>  </tr>  </thead>  <tbody> ";
 $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
     $.each(json, function(arrayID,message) {
         $.each(message, function() {
-            table += "<tr><td><a href=\"#" + message.conversation.id + "\">test</a></td>";
+            table += "<tr><td> <input type=\"checkbox\"></td>";
+
+            table += "<td><a href=\"#" + message.conversation.id + "\">test</a></td>";
             table += "<td>" + message.conversation.subject + "</td>";
             table += "<td>" + message.conversation.updated_at + "</td></tr>";
         });
