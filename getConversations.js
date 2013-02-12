a9f0cf711aaacaf7d0f936fbedcdc051d3105e93
HenryHoggard <hoggardh@cardiff.ac.uk>
2013-02-12 09:04:39 +0000

    Made username work with regex

--- getConversations.js
+++ getConversations.js
@@ -5,14 +5,32 @@
 // defines the table variable and adds the top of the table headers
 var table ="<table class=\"table\" data-provides=\"rowlink\"  >  <thead>  <tr><th></th>  <th>From</th>  <th>Subject</th>   <th>Date</th>  </tr>  </thead>  <tbody> ";
 // grabs the json of the conversations
+var username = '';
 $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
 // loop for each message
     $.each(json, function(arrayID,message) {
         $.each(message, function() {
-            // add checkbox column 
+            // add checkbox column
+            $.ajax({
+            async: false,
+            type: 'GET',
+            url: 'https://pod.cscf.me/conversations/'+message.conversation.id,
+            success: function(data) {
+                var matches = data.match(/" class='author from' >(.*)<\/a>/);
+                if (matches != null) {
+                    username = matches[1];
+                }
+                else {
+                    username = "Me";   
+                }
+
+
+            }
+            }); 
+          
             table += "<tr><td> <input type=\"checkbox\"></td>";
             // adds link to message (for james to create the page for)
-            table += "<td><a href=\"#" + message.conversation.id + "\">Username</a></td>";
+            table += "<td><a href=\"#" + message.conversation.id + "\">"+ username + "</a></td>";
             // add subject and date
             table += "<td>" + message.conversation.subject + "</td>";
             table += "<td>" + message.conversation.updated_at + "</td></tr>";
@@ -26,4 +44,12 @@ $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
 
 });
 
-
+/*
+$.get('https://pod.cscf.me/conversations/', function(data) {
+    $('.result').html(data);
+    
+    var linkRegex = /a class='conversation' href='\/conversations\/(.*)'/;
+    var matches = linkRegex.exec(data);
+    
+});
+*/
