dff624ee811844ad07681acda877f225364b0c40
SMJ93 <stuartjones93@hotmail.com>
2013-02-15 22:59:03 +0000

    V2

--- sendmessage.js
+++ sendmessage.js
@@ -1,14 +1,42 @@
-var  to =  document.getElementsByName('username')
-var subject = document.getElementsByName('title')
-var content = document.getElementsByName('content')
 
-$("sendmessage").click(function(data){
-			$.POST("wk3.org/conversations", {
-				"authenticity_token":"vGShXUOy4i5C/z53CFCs/zmLfpwUMPewekY/uXTb/hI=",
-				"contact_autocomplete": "",
-				"contact_ids": " ,,16633",
-				"conversation[subject]": " It Worked!",
-				"conversation[text]": " hahah",
-				"commit":"Send",
-		});
+
+
+
+
+
+
+// variable to hold request
+var request;
+// bind to the submit event of our form
+$("#foo").submit(function(event){
+    // abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+    // let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+    // serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // let's disable the inputs for the duration of the ajax request
+    $inputs.prop("disabled", true);
+
+    // fire off the request to /form.php
+    var request = $.ajax({
+        url: "https://pod.cscf.me/conversations",
+        type: "post",
+        data:	serializedData
     });
+
+    // callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // prevent default posting of form
+    event.preventDefault();
+});
