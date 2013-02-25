7cb169a2c16697e8c4c0eecdc156dd9ebb6c3060
Ryan <Ryanmc44567@gmail.com>
2013-02-25 23:29:33 +0000

    Proof of Concept

--- getGroups.js
+++ getGroups.js
@@ -2,18 +2,39 @@
 // By Ryan McIntyre - Based on getConversations.js
 
 	var Group = '';
+	var table ="<table class=\"table\" data-provides=\"rowlink\" > <thead> <tr><th>IWroteThis</th></tr> </thead> <tbody> ";
+	var myRe = /class='aspect_selector' href='(.*)'/g;
+	var matches;
 
 	$.ajax({
 		async: true,
         type: 'GET',
         url: 'https://pod.cscf.me/contacts/',
         success: function(data) {
-                    var matches = data.match(/"window.current_user_attributes = (.*)\n<\/script>/); // regex to extract it,
-                    var Group = matches[1];
+
+			while ((matches = myRe.exec(data)) !== null)
+			{
+
+			  var X = myRe.lastIndex;
+			  console.log(data.indexOf("<",X));
+			  var Temp = data.indexOf("<",X);
+			  Group = data.substring(X+1,Temp);
+
+
+			  table += "<tr>";
+			  table += "<td>" + Group + "</td>";
+			  table += "</tr>";
+			  }
+
+
+
+		    		table += "</tbody></table>";
+
+					document.getElementById('table2').innerHTML = table;
 		}
+
 	});
 
-     	document.getElementById('table1').innerHTML = Group;
 
 
 
