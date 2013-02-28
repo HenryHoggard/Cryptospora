2aedbdc20bdd0cbd429e4c7c1e139becb5d47ac1
SMJ93 <stuartjones93@hotmail.com>
2013-02-28 16:48:25 +0000

    WORKING V1

--- sendmessage.js
+++ sendmessage.js
@@ -1,38 +1,60 @@
-
-
-
-
-
-
-
 // variable to hold request
 var request;
+
 // bind to the submit event of our form
-$("#foo").submit(function(event){
+$("#send").submit(function(event){
     // abort any pending request
     if (request) {
         request.abort();
     }
     // setup some local variables
-    var $form = $(this);
+	var $form = $(this);
     // let's select and cache all the fields
     var $inputs = $form.find("input, select, button, textarea");
-    // serialize the data in the form
-    var serializedData = $form.serialize();
 
-    // let's disable the inputs for the duration of the ajax request
-    $inputs.prop("disabled", true);
+	
+	var token = getToken();
+	var id = $('#id1').val();
+	var subject = $('#subject1').val();
+	var text = $('#text1').val();
+	var utf = "%E2%9C%93";
+	var cont = "";
+	var com = "Send";
+	
+	var data = new FormData();
+	data.append('utf8',utf);
+	data.append('contact_autocomplete',cont);
+	data.append('contact_ids', id);
+	data.append('conversation[subject]', subject);
+	data.append('conversation[text]', text);
+	data.append('commit',com);
+	data.append('authenticity_token', token);
 
-    // fire off the request to /form.php
-    var request = $.ajax({
+    // post the data
+    var request = $.ajax
+	({
         url: "https://pod.cscf.me/conversations",
         type: "post",
-        data:	serializedData
+		data:  data,
+
+		processData:false,
+	    contentType: false,
+		async:false,
+		//"utf8: "+utf+
+				//"\n"+"contact_autocomplete: "+cont+
+				//"\n"+"contact_ids: "+id+
+				//"\n"+"conversation[subject]: " +subject+
+				//"\n"+"conversation[text]: " +text+
+				//"\n"+"commit: "+com+
+				//"\n"+"authenticity_token: "+token
+				//{utf8: utf, contact_autocomplete: cont, contact_ids: id, conversation[subject]: subject, conversation[text]: text, commit: com, authenticity_token: token}
+				
     });
 
     // callback handler that will be called regardless
     // if the request failed or succeeded
-    request.always(function () {
+    request.always(function () 
+	{
         // reenable the inputs
         $inputs.prop("disabled", false);
     });
@@ -40,3 +62,23 @@ $("#foo").submit(function(event){
     // prevent default posting of form
     event.preventDefault();
 });
+
+
+
+ function getToken() 
+ {       
+	$.ajax
+	({
+        async: false,
+        type: 'GET',
+        url: 'https://pod.cscf.me/conversations',
+        success: function(data) 
+		{
+			var matches = data.match(/<meta content="(.*)" name="csrf-token" \/>/); // regex to extract it,
+			// if there are no matches, it must be a self message 
+			tok = matches[1];
+			result = tok;
+		}
+	});	
+	return result;
+}
