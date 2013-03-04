7b32103a2088f3f546592d2245b69924e83f00aa
HenryHoggard <hoggardh@cardiff.ac.uk>
2013-03-04 14:04:30 +0000

    final commit

--- getConversations.js
+++ getConversations.js
@@ -5,12 +5,13 @@
 // defines the table variable and adds the top of the table headers
 var table ="<table class=\"table\" data-provides=\"rowlink\"  >  <thead>  <tr><th></th> <th></th> <th>From</th>  <th>Subject</th>   <th>Date</th>  </tr>  </thead>  <tbody> ";
 // grabs the json of the conversations
-var username = '';
+var username = [];
     $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
     // loop for each message
     var progress = 0;
     var iter = 0;
     var length = 0;
+    var i = 0;
         $.each(json, function(arrayID,message) {
             $.each(message, function() {
                 // for every message, extract author of message,                               
@@ -22,32 +23,36 @@ var username = '';
                     var matches = data.match(/" class='author from' >(.*)<\/a>/); // regex to extract it,
                     // if there are no matches, it must be a self message 
                     if (matches != null) {
-                        username = matches[1];
+                        username[i] = matches[1];
                     }
                     else {
-                        username = "Me";   
+                        username[i] = "Me";   
                     }
                        table += "<tr><td> <input type=\"checkbox\"></td>";
                 // adds link to message (for james to create the page for)
                 table += "<td><img src=\"https://wk3.org/assets/user/default.png\" height=\"24\" width=\"24\"></td>";
-                table += "<td><a href=\"viewDecrypt.html?messageid=" + message.conversation.id + "\">"+ username + "</a></td>";
+                table += "<td><a href=\"viewDecrypt.html?messageid=" + message.conversation.id + "\">"+ username[i] + "</a></td>";
                 table += "<td>" + message.conversation.subject + "</td>";
                 table += "<td>" + message.conversation.updated_at + "</td></tr>";
                 length = json.length;
                 iter = (100/length);
                 iter = Math.ceil(iter * 10) / 10;
-                console.log(iter);
                 progress = (progress+iter);
-                console.log(progress);
-                $("#status").html("Downloading Message " + message.conversation.id);
+
+                $("#status").html("Loading Progess " + progress + "%");
                 $("#progress").css('width',progress+'%');
+                i++;
+                if (i == 9) {
+                                          
+                    
+                }
                 if (progress > 99.999) {
                        table += "</tbody></table>";
       // add table to div 'table1'
                        document.getElementById('table1').innerHTML = table;
                        $('#progressouter').remove();
                        $('#status').remove();
-
+                       pageLoaded(length);
 
                 }
                 }
@@ -72,7 +77,19 @@ $.get('https://pod.cscf.me/conversations/', function(data) {
 });
 */
 
-function deleteMessage(messageid) {
+/* function pageLoaded() {
+    console.log(Math.round(length/10);
+    $('#pagination').bootpag({
+        total: 2,
+        page: 1,
+        maxVisible: 1
+     ).on('page', function(event, num){
+    $("#content").html("Page " + num); // or some ajax content loading...
+    });
+
+}
+*/
+/*function deleteMessage(messageid) {
     token = getToken;
     $.ajax({
         type: "POST",
@@ -82,7 +99,7 @@ function deleteMessage(messageid) {
         console.log("deleted");
     });
 
-}
+}*/
 
 
 
@@ -102,7 +119,10 @@ function getToken() {
     });	
 	return result;
 }
-
+/*
 $("#delete").click(function() {
 deleteMessage(39);
-});
+});*/
+
+
+
