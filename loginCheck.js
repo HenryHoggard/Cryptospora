de9438849cf3a5a8502002004e3d146cde86965f
Sam Douthwaite-Robinson <sambuzzwick@googlemail.com>
2013-03-04 13:35:59 +0000

    Revert "directory chang"
    This reverts commit f8677d60bafbc8d064f924e7a94afeda514d6e90.

--- loginCheck.js
+++ loginCheck.js
@@ -1,6 +1,23 @@
+$(function() {
+	$('#commit').click(function() { // press Login
 
-	  // check if user is logged in by accessing sign in page
-	  function loginVer() 
+	  var username = $('#username').val();
+	  var password = $('#password').val();   	  
+	 //var username = document.getElementsByName('username');
+	 //var password = document.getElementsByName('password');
+	 //var username = JSON.stringify(username);
+	 //var password = JSON.stringify(password);
+	  //alert(username);
+	 // alert(password);
+	  //var username = "Sam";
+	  //var password = "worth22";
+	  //var authenticity_token = "taMUOrX1Q4Cx9wBXQ+6bRo9Ssw+scxxWD+HrScXArR0=";
+	  //var utf8 = "E29C93";
+	  
+	  
+	  
+	  // get an authenticity token
+	  function getToken() 
 		{ 
 		$.ajax
 		({
@@ -9,11 +26,10 @@
 		url: 'https://pod.cscf.me/users/sign_in',
 		success: function(data) 
 		{
-		var matches = data.match('You are already signed in.'); // regex to extract it,
+		var matches = data.match(/<meta content="(.*)" name="csrf-token" \/>/); // regex to extract it,
 		// if there are no matches, it must be a self message 
 		tok = matches[1];
 		result = tok;
-		alert(result);
 		}
 		});	
 		return result;
@@ -25,4 +41,69 @@
 	 // utf8=%E2%9C%93&authenticity_token=JVMo0sD9Qww93i58KCe98vrrZpuHKqYQ4CPO8RMFXAc%3D&user%5Busername%5D=sam&user%5Bpassword%5D=worth22&commit=Sign+in
 	  
 	  
+	  //var pod = document.getElementsByName('podName');
 	  
+	   if (username.value == "") {
+			$('#loginError').fadeOut();
+			$('#loginError').empty();
+			$('#loginError').append('<div class="alert alert-error"> <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter an email address!</div>');
+			$('#loginError').fadeIn();
+	   }
+	   else if (password.value == "") {
+			$('#loginError').fadeOut();
+			$('#loginError').empty();
+			$('#loginError').append('<div class="alert alert-error"> <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a password</div>');
+			$('#loginError').fadeIn();
+	   }
+	   // if all data is present...
+	   else{
+		   
+			 //post the login details
+			var dataToSend = 'authenticity_token=' + authenticity_token +'&user[username]=' + username +'&user[password]='+ password + '&commit=Sign+in';
+			 var dataToSend2 = JSON.stringify(dataToSend);
+			//alert(dataToSend2);
+					//var dataToSend = 'utf8=E29C93&authenticity_token=Wpy61blxjdZx3K8tB9aArqUZOaBwfl5eCBNNM235j34=&user[username]=sam&user[password]=worth22&commit=Sign+in';
+					//alert (dataToSend);
+					 $.ajax({                
+					   url: "https://pod.cscf.me/users/sign_in", 
+					   type: "POST",
+					   data: dataToSend,     
+					   cache: false,
+					   
+					   /*success: function (phpOutput) {
+							//$('#testDummy').append('login works');
+							jQuery('#testDummy').append('<h2>Test Phase Complete.</h2><h2>Please Wait...</h2><img src="loader.gif" title="Working..."/>');
+						},*/
+						success: function(php_output) {
+							 $("#testDummy").html(php_output);
+							// check if login was successfull
+							
+							var matches = php_output.match('<div class="message">Invalid email or password.</div>'); // regex to extract it,
+							// if there are no matches, it must be a self message 
+							tok = matches[1];
+							if (tok == "Invalid email or password.")
+							{
+								$('#loginError').fadeOut();
+								$('#loginError').empty();
+								$('#loginError').append('<div class="alert alert-error"> <button type="button" class="close" data-dismiss="alert">&times;</button>Invalid Username or Password</div>');
+								$('#loginError').fadeIn();
+							}
+							
+				   },
+						
+					error: function(){
+						$('#loginError').fadeOut();
+						$('#loginError').empty();
+						$('#loginError').append('<div class="alert alert-error"> <button type="button" class="close" data-dismiss="alert">&times;</button>Connection Error. Contact Support or restart extention.</div>');
+						$('#loginError').fadeIn();
+				
+					}
+			
+					}); 
+        
+	
+		}
+		
+	  return false;
+	});
+});
