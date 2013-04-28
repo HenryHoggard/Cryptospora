c1bc1aa4049a30b20bbf7ebeee202812224fe2be
James White <jamesw1013@googlemail.com>
2013-04-28 19:36:06 +0100

    updated functionality

--- ViewMessage.js
+++ ViewMessage.js
@@ -1,7 +1,7 @@
 // Populate view message with a message in Diaspora inbox
  
  
- 
+
 var username = '';
 var subject = '';
 var dateTime = '';
@@ -10,8 +10,10 @@ var replyMessage='';
 
 var token = getToken();
 
+var messageid= $.url().param('messageid');
 // variable to hold request
 var request;
+
  
 $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
     $.each(json, function(arrayID,message) {
@@ -20,7 +22,7 @@ $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
         $.ajax({
             async: true,
             type: 'GET',
-            url: 'https://pod.cscf.me/conversations/' +message.conversation.id,
+            url: 'https://pod.cscf.me/conversations/' +messageid,
             success: function(data) {
                 var matches = data.match(/" class='author from' >(.*)<\/a>/); // regex to extract it,																			// if there are no matches, it must be a self message
 				var matches2 = data.match(/<div class='ltr'>\n<p>(.*)<\/p>/); //regex to extract message content,
@@ -36,7 +38,40 @@ $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
 		dateTime = message.conversation.updated_at;
 		content = matches2[1]; 
  
- 
+ 		$("#delete").click( function()
+           {
+             alert('You have deleted this conversation');
+			 if (request1) {
+				request1.abort();
+			 }
+			 
+			 
+			var $form = $(this);			 
+			var utf = "%E2%9C%93"; 
+			var token = getToken();
+			var referer = messageid;
+			var method = "delete";
+			
+			var data1 = new FormData();
+			data1.append('utf8', utf);
+			data1.append('authenticity_token', token);
+			data1.append('Referer', referer);
+			data1.append('_method', method);
+			
+			var requestDelete = $.ajax({
+				url: 'https://pod.cscf.me/conversations/'+messageid+'/visibility',
+				type: "post",
+				data: data1,
+					
+				processData: false,
+				contentType: false,
+				async: false,
+				
+
+			});
+			window.open('inbox.html', '_self', false);
+			}
+        );
  
         document.getElementById('testSubject').value = subject;
 		document.getElementById('userField').value = username;
@@ -71,7 +106,7 @@ $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
 			
 			 // fire off the request to /form.php
 				var request1 = $.ajax({
-					url: 'https://pod.cscf.me/conversations/9/messages',
+					url: 'https://pod.cscf.me/conversations/'+messageid+'/messages',
 					type: "post",
 					data: data,
 					
@@ -99,6 +134,7 @@ $.getJSON("https://pod.cscf.me/conversations.json", function(json) {
 	
 });
 
+
 function getToken() 
 { 
 $.ajax({
@@ -113,4 +149,10 @@ $.ajax({
 				}
 			});	
 		return result;
-	}			
+	}		
+
+
+
+		
+var url = $.url(true).fparam('messageid');
+console.log(url);	
