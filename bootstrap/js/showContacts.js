5430c6b37e4c5e50b1da8aecf3ea06c720511877
Ryan <Ryanmc44567@gmail.com>
2013-04-28 21:23:25 +0100

    Del Contacts

--- bootstrap/js/showContacts.js
+++ bootstrap/js/showContacts.js
@@ -15,7 +15,7 @@ for (var i = 0; i < ArrayOfContacts.length; i++) {
 		store += "<div id=" + "\"" + i + "\"" + " class=\"accordion-body collapse\"><div class=\"accordion-inner\">";
 		store += "<form><fieldset><a class=\"btn pull-right\" id=\"" + ArrayOfContacts[i]["ContactAddress"] + "\">Message</a><br />Groups:";
 			for(var index in ArrayOfContacts[i]) {
-				if(index != "ContactName" && index != "ContactAddress"){
+				if(index != "ContactName" && index != "ContactAddress" && index !="ContactID"){
 			  	store += "<div class=\"alert alert-success\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>";
 			  	store += index;
 			  	store += "</div>"
@@ -35,4 +35,97 @@ for (var i = 0; i < ArrayOfContacts.length; i++) {
 }
 document.getElementById('table1').innerHTML = store;
 
-// <script src="getContactsV2.js"></script>
+
+
+
+$("#Delete").click( function()
+          {
+            //alert('This Works');
+            console.log("The Delete Button has been clicked");
+
+            var $form = $(this);
+            var anMethod = "DELETE";
+            var aspect = getAspectID();
+            var person = getUserID();
+            var utf = "%E2%9C%93";
+			var token = getToken();
+			var Ref = 'https://pod.cscf.me/contacts';
+
+
+            var dataRemoveFromAspect = new FormData();
+            dataRemoveFromAspect.append('utf8', utf);
+            dataRemoveFromAspect.append('Referer', Ref);
+            dataRemoveFromAspect.append('aspect_id', aspect);
+            dataRemoveFromAspect.append('person_id', person);
+            dataRemoveFromAspect.append('_method', anMethod);
+
+            var request = $.ajax({
+				url: 'https://pod.cscf.me/aspect_memberships/42.json',
+				type: "post",
+				beforeSend: function (xhr) {
+				     xhr.setRequestHeader('X-CSRF-Token', token);
+				},
+
+				data: dataRemoveFromAspect,
+
+				processData: false,
+				contentType: false,
+				async: false,
+
+			});
+});
+
+function getUserID()
+{
+	var UEmail = document.getElementById('UserEmail').value;
+
+	console.log("I am Trying to Convert the Email ID of : " + UEmail);
+
+	for (var i = 0; i < ArrayOfContacts.length; i++) {
+		console.log("Looks for both: " + ArrayOfContacts[i]["ContactAddress"] + ArrayOfContacts[i]["ContactName"]);
+		if(ArrayOfContacts[i]["ContactAddress"] == UEmail || ArrayOfContacts[i]["ContactName"] == UEmail)
+		{
+			var UserID = ArrayOfContacts[i]["ContactID"];
+			console.log("Found :" + UserID);
+			return(UserID);
+		}
+	}
+	//alert('The Contact was not found - no Change will take place');
+}
+
+function getAspectID()
+{
+	var Aspect = document.getElementById('AspectName').value;
+	var ArrayOfAspects = JSON.parse(localStorage.StoredArrayGroups);
+
+	console.log("I am Trying to Convert the Aspect ID of: " + Aspect);
+
+	for (var i = 1; i < ArrayOfAspects.length; i++) {
+		console.log("Looks for: " + ArrayOfAspects[i]["GroupName"]);
+		if(ArrayOfAspects[i]["GroupName"] == Aspect)
+		{
+			var AspectID = ArrayOfAspects[i]["GroupURL"];
+			console.log("Found :" + AspectID)
+			var Temp = AspectID.indexOf("=");
+			AspectID = AspectID.substring(Temp+1, AspectID.length);
+			return(AspectID);
+		}
+	}
+	 //alert('The Aspect was not found - no Change will take place');
+}
+
+function getToken()
+{
+	$.ajax({
+		async: false,
+		type: 'GET',
+		url: 'https://pod.cscf.me/conversations',
+		success: function(data) {
+			var matches = data.match(/<meta content="(.*)" name="csrf-token" \/>/); // regex to extract it,
+			tok = matches[1];
+			result = tok;
+		}
+	});
+return result;
+}
+
